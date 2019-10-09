e58591dec8c31dddac940aece91877355e09ffe3
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-09 14:48:20 +0530

    updating the search box

--- script.js
+++ script.js
@@ -20,30 +20,13 @@ document.getElementsByClassName("search-button")[0].addEventListener("click", fu
     skill = $("#skills").val();
     locationn = $("#location").val();
     experience = $("#experience").val();
-    // experience = "";
-
-    console.log(skill+ " "+ locationn + " " + experience)
-
-    // userSearch=jsonData.data.filter( (value) =>  value.skills === skill && value.location === locationn && value.experience === experience )
-    // userSearch=jsonData.data.filter( (value) =>  (skill != "" && skill != undefined) ? (value.skills === skill) : value.skills )
-    console.log((skill != "" ) ? "Not Empty" : "Empty");
-    // console.log(userSearch)
-
-    userSearch=jsonData.data.filter( (value) =>  ( (skill != "" && skill != undefined) ? (value.skills === skill) : value.skills ) && 
+    userSearch=jsonData.data.filter( (value) =>  ( ( (skill != "" && skill != undefined) ? (value.skills === skill) : value.skills ) && 
                                                  ( (locationn != "" && locationn != undefined) ? (value.location === locationn) : value.location) && 
-                                                 ( (experience != "" && experience != undefined) ? (value.experience === experience) : value.experience) )
-
-                            // .filter( (value) => value.location === locationn)
-                            // .filter( (value) => value.experience === experience)
+                                                 ( (experience != "" && experience != undefined) ? (value.experience === experience) : value.experience) ))
     console.log(userSearch)
-    // userLocation=userSkill.filter((value)=>value.location ===locationn)
-    // console.log(userLocation)
-    
-    console.log(jsonData)
+    let n=userSearch.length
+    for(let i =0; i <n ; i++){
+        console.log(userSearch[i])
+    }
 });
 
-function passing(jsonData){
-    this.jsonData=jsonData
-    
-    
-}
