dcd9c8183c885f1292939937907f75091e9bb55e
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-10 12:24:25 +0530

    creating the filter option

--- script.js
+++ script.js
@@ -23,22 +23,27 @@ function init(){
 function getUserInput(){
     document.getElementsByClassName("search-button")[0].addEventListener("click", function() {
         skill = $("#skills").val().toLowerCase();
-        console.log(skill)
-        if(skill ===""){
-            alert("Enter the valued input")
-        }
-        else{
-            userSearch=jsonData.data.filter( (value) => (value.skills.toLowerCase().includes(skill)) || 
-                                                        (value.location.toLowerCase().includes(skill)) ||
-                                                        (value.experience.toLowerCase().includes(skill)))
-                                                    
-            console.log(userSearch)
-        }
-        let n=userSearch.length
-        if(n === 0){
-            alert("Enter the valued input")
-        }
+        displayResult()
+    })
+}
 
+function displayResult(){
+    if(skill ===""){
+        alert("Enter the valued input")
+    }
+    else{
+        userSearch=jsonData.data.filter( (value) => (value.skills.toLowerCase().includes(skill)) || 
+                                                    (value.location.toLowerCase().includes(skill)) ||
+                                                    (value.experience.toLowerCase().includes(skill)))
+                                                
+        
+    }
+    let n=userSearch.length
+    if(n === 0){
+        alert("Enter the valued input")
+    }
+    else{
+        $(".display-job").show();
         userSearch.forEach(userSearch =>{
             $('.display-job').append(`<div class="header">
             <span class="title">${userSearch.title}</span>
@@ -48,6 +53,26 @@ function getUserInput(){
         <span class="location">${userSearch.location}</span>
         </div>`)
         })
-    });
+    }
+
+    filterOption()
 }
 
+
+function filterOption(){
+    $(" .filter-area").show();
+    let filterLocation = new Array()
+    console.log(userSearch)
+    console.log(filterLocation)
+
+    let n=userSearch.length
+    for(let i=0;i<n;i++)
+    {
+        let ffilterLocation=userSearch[i].location
+        filterLocation.push(ffilterLocation)
+        
+    }
+    console.log(filterLocation)
+    // filterLocation = userSearch.forEach((value) => value.location)
+    
+}
